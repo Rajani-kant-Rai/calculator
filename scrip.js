f87f9943dd1e317c246a4b117eac1f2c21505157
Rajni-Kant-Rai <monurai8059@gmail.com>
2025-02-04 00:46:19 +0530

    Indent updsated

--- scrip.js
+++ scrip.js
@@ -1,7 +1,10 @@
 let input = document.getElementById('inputbox');
 let buttons = document.querySelectorAll('button');
+
 let string = "";
+
 let arr = Array.from(buttons);
+
 arr.forEach(button => {
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML == '='){
